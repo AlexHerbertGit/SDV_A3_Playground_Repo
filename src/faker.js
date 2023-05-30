ab42c8831d4b536bd78a77b5282449eead6cbf25
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-05-31 11:33:21 +1200

    Set up Faker code for creating a user

--- src/faker.js
+++ src/faker.js
@@ -1,2 +1,15 @@
 //Faker.JS code
+//npm install --save-dev @faker-js/faker
 
+const faker = require("faker")
+const fs = require("fs")
+
+ const createUser = () => {
+    return {
+        id: faker.datatype.uuid(),
+        image: faker.image.image(),
+        fullName: fullName.fullName.fullName(),
+        bio: faker.person.bio(),
+        phone: faker.phone.imei()
+    }
+ }
